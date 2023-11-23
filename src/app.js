0cbdfed1edc25e181150e35d6124c229c0ad2508
Max3796 <gurpreet.singh@kalvium.community>
2023-11-23 10:48:08 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,44 @@
 class Blog {
   // Progression 1: Add 2 methods - addTitle(), and addDescription()
+  constructor(title,detail){
+    this.title = title
+    this.detail = detail
+  }
+  addTitle(){
+    var title_card = document.createElement('h1');
+        title_card.setAttribute("id","blog-title");
+        console.log(title_card);
+        document.getElementById('card-text').appendChild(title_card);
+        title_card.innerHTML += this.title;
+  }
+  addDescription(){
+    var description = `<p id = "blog-description">${this.detail}</p>`
+    document.getElementById('card-text').innerHTML+=description  
 }
+}
+// var Yeah = new Blog("Yeah Yeah", "Yo Yo Yeahhhhh")
 
 // Progression 2: Setup an event listner - add two functions
 // 1. `helperAddPost()`
+var isOpen=false
+function helperAddPost(){
+  let popup = document.getElementById("popupContact")
+  popup.style.display = "inline"
+  if(isOpen){
+    popup.style.display = "none"
+    isOpen = false
+  }else{
+    isOpen = true
+  }
+}
 // 2. `helperPost()`
+function helperPost(){
+  var title = document.getElementById("title").value
+  var description = document.getElementById("detail").value
+  var blog = new Blog(title,description)
+  blog.addTitle()
+  blog.addDescription()
+  document.getElementById("title").value = ''
+  document.getElementById("detail").value = ''
+  helperAddPost()
+}
